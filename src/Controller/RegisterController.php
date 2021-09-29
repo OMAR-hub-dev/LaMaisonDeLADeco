a015eba65313504570ed5c5c78dd9fcebe96961e
o.omar <oulaya.omar@gmail.com>
2021-09-29 14:32:58 +0200

    login, register, security,

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
@@ -18,13 +19,15 @@ class RegisterController extends AbstractController
         $this->entityManger = $entityManager;
     }
     #[Route('/inscription', name: 'register')]
-    public function index(Request $request) : Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder) : Response
     {
         $user= new User();
         $form =$this->createForm(RegisterType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
+            $password= $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
             $this->entityManger->persist($user);
             $this->entityManger->flush();
         }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -19,6 +21,14 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstName',TextType::class,[
                         'label'=>'Votre prenom',
+                        'constraints'=> new Length([
+                                'min' => 2,
+                                'minMessage' => 'votre prenom doit contenir au minimum {{ limit }} caractères ',
+                                // max length allowed by Symfony for security reasons
+                                'max' => 20,
+                                'maxMessage' => 'votre prenom doit contenir moin de  {{ limit }} caractères ',
+                            ]),
+                        
                         'attr'=>
                             ['placeholder'=>'. . . merci de saisir votre prenom']])
             ->add('lastName',TextType::class,[
@@ -30,13 +40,32 @@ class RegisterType extends AbstractType
                 'attr'=>
                     ['placeholder'=>'. . . merci de saisir votre email']])
             ->remove('roles')
-            ->add('password',RepeatedType::class,[
-                'type'=>PasswordType::class,
+            ->add('password', RepeatedType::class, [
+                'type'=>PasswordType::class, 
                 'invalid_message'=>'le mot de passe et la confiramation doivent être identique',
                 'label'=>'Votre password',
                 'required'=>true,
-                'first_options'=>['label'=>'mot de passe'],
-                'second_options'=>['label'=>'confirmer votre mot de passe'],
+                'first_options'=>[
+                    'label'=>'mot de passe',
+                    'attr'=>
+                    ['placeholder'=>'. . . merci de saisir votre mot fde passe']
+                    ],
+                'second_options'=>[
+                    'label'=>'confirmer votre mot de passe',
+                    'attr'=>
+                    ['placeholder'=>'. . . merci de confirmer votre mot de passe']
+                    ],
+                'constraints'=>[
+                    new NotBlank([
+                    'message' => 'veuillez entrer votre mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'votre mot de passe doit contenir au minimum {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 26,
+                    ]),
+                ]
             ])
             
       
