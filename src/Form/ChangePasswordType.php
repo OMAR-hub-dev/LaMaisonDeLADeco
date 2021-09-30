a2cdf3132da71faaaba258d75f956588701eb195
o.omar <oulaya.omar@gmail.com>
2021-09-30 13:11:30 +0200

    suite

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName',TextType::class,
+            ['disabled'=>true,
+            'label'=>'Mon prénom'])
+            ->add('lastName',TextType::class,
+            ['disabled'=>true,
+            'label'=>'Mon nom'])    
+            ->add('email',EmailType::class,[
+                'disabled'=>true,
+                'label'=>'Mon adress email'
+            ])
+            ->remove('roles')
+            ->add('old_password',PasswordType::class,[
+                'mapped'=>false,
+                'label'=>'Mot de passe actuel',
+                'attr'=>[
+                    'placeholder'=>'Veuillez saisir votre mot de passe actuel'
+                ]
+            ] )
+            ->add('new_password', RepeatedType::class, [
+                'type'=>PasswordType::class, 
+                'mapped'=>false,
+                'invalid_message'=>'le mot de passe et la confiramation doivent être identique',
+                'label'=>'Votre password',
+                'required'=>true,
+                'first_options'=>[
+                    'label'=>'Mon nouveau mot de passe',
+                    'attr'=>
+                    ['placeholder'=>'. . . merci de saisir votre nouveau mot de passe']
+                    ],
+                'second_options'=>[
+                    'label'=>'confirmer votre mot de passe',
+                    'attr'=>
+                    ['placeholder'=>'. . . merci de confirmer votre nouveau mot de passe']
+                    ],
+                'constraints'=>[
+                    new NotBlank([
+                    'message' => 'veuillez entrer votre mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'votre mot de passe doit contenir au minimum {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 26,
+                    ]),
+                ]
+            ])
+            ->add('submit',SubmitType::class,[
+                'label'=>'Mettre à jour'])
+        
+                
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
